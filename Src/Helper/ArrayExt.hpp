d2efcdd9692d31c301dd9297a05f2e14ad2c71a4
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2022-07-28 02:09:43 +0300

    feat: read image from http stream

---
+++ Src/Helper/ArrayExt.hpp
@@ -0,0 +1,44 @@
+namespace ArrayExt
+{
+    /// <summary>
+    /// Find first equal bytes in source
+    /// </summary>
+    /// <returns>-1 if not find in source</returns>
+    template<typename T>
+    int FindBytesIndex(
+        T* source,
+        const int& sourceSize,
+        const T* pattern,
+        const int& patternSize
+    )
+    {
+        int index = -1;
+        for (int i = 0; i < sourceSize; i++)
+        {
+            if (sourceSize - i < patternSize)
+            {
+                return index;
+            }
+
+            for (int j = 0; j < patternSize; j++)
+            {
+                if (source[i + j] == pattern[j])
+                {
+                    if (j == patternSize - 1)
+                    {
+                        index = i;
+                        return index;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
